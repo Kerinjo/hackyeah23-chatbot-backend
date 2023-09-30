9834a08f9c28689d187f1bc5341107ff73549e27
Kerinjo <konmarciniak@gmail.com>
2023-09-30 20:30:50 +0200

    possibility of db purge, temporary data models

--- API/urls.py
+++ API/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('handle_message/', views.handle_message, name='handle_message'),
     path('generate_uuid/', views.generate_uuid, name="generate_uuid"),
     path('fetch_messages/', views.fetch_messages, name="fetch_messages"),
+    path('purge/', views.purge, name="purge")
 ]
--- API/views.py
+++ API/views.py
@@ -1,9 +1,10 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .models import GeneratedUUID
+from django.core.signals import request_finished
 import json
 import uuid
 import pymongo
-from django.core.signals import request_finished
+import datetime
 
 
 client = pymongo.MongoClient("mongodb+srv://USER:eHrJbJtKHuEYZPh8@messagestorage.drak2ya.mongodb.net/?retryWrites=true&w=majority")
@@ -15,14 +16,50 @@ collection = db['Users']
 # from django.views.decorators.csrf import csrf_exempt
 #@csrf_exempt
 def handle_message(request):
+        
+        
+        
+    mock_data = {
+            "uuid" : "encepence",
+            "text" : "terefere",
+    }
+
+    temp_message = {
+        "isClient" : True,
+        "date" : datetime.datetime.now(),
+        "text" : mock_data['text'],
+    }
+    
+    print(temp_message)
+    
+
+
     if request.method == 'POST':
         try:
             # Assuming JSON
             data = json.loads(request.body.decode('utf-8'))
             message = data.get('message', '')
-
+            
             # processing the message as needed goes here
 
+            # getting json
+            # uuid, message
+            # adding datetime, isClient 
+            temp_message = {
+                    "isClient" : True,
+                    "date" : datetime.datetime.now(),
+                    "text" : message,
+                }
+            # TODO:
+            # sending to model
+            # message + history
+            # making message from what model returns
+            # adding date, isClient
+            # inserting to database
+
+
+            # returning message from client and from model
+            
             # Returning JSON response
             response_data = {'response': 'Response goes here'}
             return JsonResponse(response_data)
@@ -41,9 +78,11 @@ def generate_uuid(request):
 
         document = {
             'uuid': str(new_uuid),
-            'history': {}
+            'history': []
         }
-
+        
+        # uncomment for testing mock data
+        # document['history'] += [{"message" : "hej :)", "date" : datetime.datetime.now()}]
         collection.insert_one(document)
         
         # Return the generated UUID as a response
@@ -64,6 +103,11 @@ def fetch_messages(request):
             return JsonResponse({'error': 'User UUID not found'})
     except Exception as e:
         return JsonResponse({'error': str(e)})
+
+
+def purge(request):
+    collection.remove()
+    return HttpResponse('')
 
 
 def close_mongodb_client(sender, **kwargs):
